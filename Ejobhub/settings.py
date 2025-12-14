b17e66b47b4f38c7bc54ae9910d67103bcad390e
thomas <didymusthomas6@gmail.com>
2025-12-14 22:07:27 +0300

    Update the settings and urls for candidate app

--- Ejobhub/settings.py
+++ Ejobhub/settings.py
@@ -84,7 +84,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
